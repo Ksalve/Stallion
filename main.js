344c6dc650aca39309adbb87dad29ba58f541228
Ksalve <116504074+Ksalve@users.noreply.github.com>
2023-10-23 15:48:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("LOaded")
+const currentPage = document.getElementById("themeButton")
+if(currentPage.innerText === "Electrical")
+    currentPage.innerText = "Go Kart"
+else currentPage.innerText = "Electrical"
+
