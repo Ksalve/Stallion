8eab813c7e05b7bfbddffa4e4c97bb56f8d24d71
adii21-ux <adityasangave2112@gmail.com>
2023-11-03 15:08:43 +0530

    Theme Switch Added

--- main.js
+++
@@ -1,6 +0,0 @@
-console.log("LOaded")
-const currentPage = document.getElementById("themeButton")
-if(currentPage.innerText === "Electrical")
-    currentPage.innerText = "Go Kart"
-else currentPage.innerText = "Electrical"
-
